c14f0c28bb4c285b314b64cdcc20b7c571b9a59e
Soumya Nayak <soumyanayak.raju@gmail.com>
2023-09-13 00:09:12 +0530

    modified:   src/components/NewProject.jsx

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,15 +1,21 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { FaHtml5, FaChevronDown, FaCss3, FaJs } from "react-icons/fa";
 import SplitPane from "react-split-pane";
 import { FcSettings } from "react-icons/fc";
+import { MdCheck, MdEdit } from "react-icons/md";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
+import { Link } from "react-router-dom";
+import logo from "../assets/codepen-logo-png-transparent.png";
+import { AnimatePresence, motion } from "framer-motion";
 
 const NewProject = () => {
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
   const [outPut, setOutPut] = useState("");
+  const [title, setTitle] = useState("Untitle");
+  const [isTitle, setIsTitle] = useState("");
 
   useEffect(() => {
     updatedOutput();
@@ -34,6 +40,75 @@ const NewProject = () => {
       <div className="w-screen h-screen flex flex-col items-start justify-start overflow-hidden">
         {/* alert */}
         {/* header section */}
+
+        <header className="w-full flex items-center justify-between px-12 py-4">
+          <div className="flex items-center justify-center gap-6">
+            <Link to={"/home"}>
+              <img
+                src={logo}
+                className="object-contain w-32 h-auto"
+                alt="logo"
+              />
+            </Link>
+
+            <div className="flex flex-col items-start justify-start">
+              {/* title */}
+              <div className="flex items-center justify-center gap-3">
+                <AnimatePresence>
+                  {isTitle ? (
+                    <>
+                      <motion.input
+                        key={"titleInput"}
+                        type="text"
+                        placeholder="Your Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="px-3 py-2 rounded-md bg-transparent text-white"
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <motion.p
+                        key={"titleLabel"}
+                        className="px-3 py-2 text-white text-lg"
+                      >
+                        {title}
+                      </motion.p>
+                    </>
+                  )}
+                </AnimatePresence>
+
+                <AnimatePresence>
+                  {isTitle ? (
+                    <>
+                      <motion.div
+                        key={"MdCheck"}
+                        whileTap={{ scale: 0.9 }}
+                        className="cursor-pointer"
+                        onClick={() => setIsTitle(false)}
+                      >
+                        <MdCheck className="text-2xl text-emerald-500" />
+                      </motion.div>
+                    </>
+                  ) : (
+                    <>
+                      <motion.div
+                        key={"MdEdit"}
+                        whileTap={{ scale: 0.9 }}
+                        className="cursor-pointer"
+                        onClick={() => setIsTitle(true)}
+                      >
+                        <MdEdit className="text-2xl text-primaryText" />
+                      </motion.div>
+                    </>
+                  )}
+                </AnimatePresence>
+              </div>
+              {/* follow */}
+              <div className="flex items-center justify-center px-3 mt-2 gap-2"></div>
+            </div>
+          </div>
+        </header>
 
         {/* coding section */}
 
